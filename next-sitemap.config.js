32c2165c1245ddca0ccba8cb3d6739e1334e180a
Md Rokan Uddin <88700880+rokanuddinroman@users.noreply.github.com>
2024-02-11 21:14:01 +0600

    added index fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,19 @@
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: "https://rokan.tech",
+//   generateRobotsTxt: true, // (optional)
+//   // ...other options
+// };
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://rokan.tech",
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    // Exclude the "noindex" directive
+    additionalSitemaps: [
+      "https://rokan.tech/sitemap.xml", // Replace with the actual path to your sitemap
+    ],
+  },
   // ...other options
 };
